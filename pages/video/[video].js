f2dc67f5928cb7d5641bf9074e7a7dce67de9c40
anjali-sumfactor <anjali.sumfactor@gmail.com>
2023-05-28 20:27:47 +0530

    youtube player styling

--- pages/video/[video].js
+++ pages/video/[video].js
@@ -10,7 +10,15 @@ export default function Video() {
     }
         <Modal isOpen={true} contentLabel="Watch the video" className={styles.modal} onRequestClose={() => { router.back() }}
             overlayClassName={styles.overlay}>
-            <div>Modal body</div>
+
+            <iframe
+                className={styles.videoPlayer}
+                id="ytplayer"
+                type="text/html"
+                width="100%"
+                height="360"
+                src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com&controls=0&rel=1"
+                frameborder="0"></iframe>
         </Modal>
     </div>
 }
