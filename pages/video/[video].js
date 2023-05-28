9ff0652e6cb385b19d8d032462ef348102b33512
anjali-sumfactor <anjali.sumfactor@gmail.com>
2023-05-28 18:31:45 +0530

    install react modal component

--- pages/video/[video].js
+++ pages/video/[video].js
@@ -1,8 +1,17 @@
 import { useRouter } from 'next/router';
+import Modal from 'react-modal';
+Modal.setAppElement('#__next');
+import styles from '../../styles/Video.module.css';
 
 export default function Video() {
     const router = useRouter();
     console.log(router);
 
-    return <div>video page {router.query.video}</div>
+    return <div>video page {router.query.video
+    }
+        <Modal isOpen={true} contentLabel="Watch the video" onRequestClose={() => { router.back() }}
+            overlayClassName={styles.overlay}>
+            <div>Modal body</div>
+        </Modal>
+    </div>
 }
