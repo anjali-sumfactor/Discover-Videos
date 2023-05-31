5ce142c7ce102d9603eed3eca20c8a61292fd723
anjali-sumfactor <anjali.sumfactor@gmail.com>
2023-05-31 17:31:22 +0530

    login API create auth header

--- pages/api/login.js
+++ pages/api/login.js
@@ -5,12 +5,11 @@ export default async function login(req, res) {
         try {
             const auth = req.headers.authorization;
             const didToken = auth ? auth.substr(7) : '';
-            console.log({ token });
+            // console.log({ token });
 
             const metadata = await magicAdmin.users.getMetadataByToken(didToken);
 
             console.log({ metadata });
-
 
             //invoke magic
             res.send({ done: true });
@@ -21,27 +20,4 @@ export default async function login(req, res) {
     } else {
         res.send({ done: false });
     }
-
-
-
-
-
-    // if (req.method === "POST") {
-    //     try {
-    //         const auth = req.headers.authorization;
-    //         const didToken = auth ? auth.substr(7) : "";
-    //         console.log({ didToken });
-
-    //         const metadata = await magicAdmin.users.getMetadataByToken(didToken);
-    //         console.log({ metadata });
-
-    //         res.send({ done: true });
-
-    //     } catch (error) {
-    //         console.error("Something went wrong logging in", error);
-    //         res.status(500).send({ done: false });
-    //     }
-    // } else {
-    //     res.send({ done: false });
-    // }
 }
