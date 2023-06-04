50303da32092233ca59ba7f8ba62698f3e51c3c5
anjali-sumfactor <anjali.sumfactor@gmail.com>
2023-06-04 20:19:20 +0530

    use query string for video ID

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,19 +1,20 @@
 import jwt from 'jsonwebtoken';
+import { findVideoIdByUser } from '@/lib/db/hasura';
 
 export default async function stats(req, resp) {
     if (req.method === "POST") {
-        console.log({ cookies: req.cookies });
 
         try {
             const token = req.cookies.token;
             if (!token) {
                 resp.status(403).send({});
             } else {
-                const decoded = jwt.verify(token, process.env.JWT_SECRET);
-                console.log({ decoded });
+                const videoId = req.query.videoId;
+                const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
 
-                const findVideoId = await findVideoIdByUser();
-                resp.send({ msg: " it works", decoded });
+                const userId = decodedToken.issuer;
+                const findVideoId = await findVideoIdByUser(token, userId, videoId);
+                resp.send({ msg: " it works", decodedToken, findVideoId });
             }
         } catch (error) {
             console.error("Error occured /stats", error);
